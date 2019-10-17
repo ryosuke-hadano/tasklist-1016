46824fcb61a1e7e6e04dbde8be264b331ebf75ae
ryosuke.hadano <ryousuke1010jp@yahoo.co.jp>
2019-10-17 14:05:52 +0000

    Procfile

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
